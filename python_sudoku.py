a92c1df8763d67aa885ad5b0203ffe47aa8945ad
Shakeal Hodge <hodges2@carleton.edu>
2022-02-01 23:57:22 -0600

    print sudoku board

---
+++ python_sudoku.py
@@ -0,0 +1,35 @@
+from dokusan import generators
+import random
+import numpy as np
+
+# A sodoku generator and solver using dokusan and np, inspired by techwithtim
+
+# Generate a 
+board = np.array(list(str(generators.random_sudoku(avg_rank=random.randint(150,450)))))
+board = board.reshape(9, 9)
+
+# Print the sudoku board from my array using dash(-) and pipe(|)
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+print_board(board)
