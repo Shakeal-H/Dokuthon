ad6a8b1cc336ce759504ea0778ed10b83e9d94d9
Shakeal Hodge <hodges2@carleton.edu>
2022-02-03 00:04:35 -0600

    backtracking algo & dokusan implementation

--- python_sudoku.py
+++ python_sudoku.py
@@ -4,13 +4,24 @@ import numpy as np
 
 # A sodoku generator and solver using dokusan and np, inspired by techwithtim
 
-board = np.array(list(str(generators.random_sudoku(avg_rank=random.randint(150,450)))))
-board = board.reshape(9,9)
-
+# Dokusan sodoku solver
 alt_board = generators.random_sudoku(avg_rank=random.randint(150,450))
 
 def print_fancier_board(sudoku):
     return print(renderers.colorful(sudoku))
+
+def solve_sudoku(some_board):
+    solution = solvers.backtrack(some_board)
+    return print(renderers.colorful(solution))
+
+
+# Create a sudoku board of random levels as an array of ints
+board = np.array(list(str(generators.random_sudoku(avg_rank=random.randint(150,450)))))
+board = [int(i) for i in board]
+board = np.array(board)
+board = board.reshape(9,9)
+
+
 
 # Print the sudoku board from my array using dash(-) and pipe(|)
 def print_board(board):
@@ -36,5 +47,47 @@ def find_empty(board):
 
     return None
 
-print_board(board)
-print_fancier_board(alt_board)
+def valid(board, number, position):
+    # Check the row of board
+    for i in range(len(board[0])):
+        if (board[position[0]][i] == number) and (position[1] != i):
+            return False
+
+    # Check the column of board
+    for i in range(len(board)):
+        if board[i][position[1]] == number and position[0] != i:
+            return False
+
+    # Check the box of board
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if board[i][j] == number and (i,j) != position:
+                return False
+
+    return True
+
+def solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(board, i, (row, col)):
+            board[row][col] = i
+
+            if solve(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+
+# print_fancier_board(alt_board)
+solve_sudoku(alt_board)
+
