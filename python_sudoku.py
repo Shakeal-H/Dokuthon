19e43f27edcef1c3e311c89b0b47f366d39f3fa4
Shakeal Hodge <hodges2@carleton.edu>
2022-02-03 12:17:58 -0600

    print_board bug fix

--- python_sudoku.py
+++ python_sudoku.py
@@ -23,16 +23,15 @@ board = board.reshape(9,9)
 
 
 
-# Print the sudoku board from my array using dash(-) and pipe(|)
+
 def print_board(board):
     for i in range(len(board)):
         if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
+            print("------------------------")
 
         for j in range(len(board[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
-
             if j == 8:
                 print(board[i][j])
             else:
@@ -43,28 +42,26 @@ def find_empty(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
-                return (i, j)  # row, col
+                # Row and column of empty cell
+                return (i, j)  
 
     return None
 
-def valid(board, number, position):
-    # Check the row of board
-    for i in range(len(board[0])):
-        if (board[position[0]][i] == number) and (position[1] != i):
+def valid_num(board, row, col, num):
+    for j in range(0, 9):
+        if board[row][j] == num:
             return False
 
-    # Check the column of board
-    for i in range(len(board)):
-        if board[i][position[1]] == number and position[0] != i:
+    for i in range(0, 9):
+        if board[i][col] == num:
             return False
 
-    # Check the box of board
-    box_x = position[1] // 3
-    box_y = position[0] // 3
+    start_row = (row // 3) * 3
+    start_col = (col // 3) * 3
 
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if board[i][j] == number and (i,j) != position:
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if board[start_row + i][start_col + j] == num:
                 return False
 
     return True
@@ -77,7 +74,7 @@ def solve(board):
         row, col = find
 
     for i in range(1,10):
-        if valid(board, i, (row, col)):
+        if valid_num(board, row, col, i):
             board[row][col] = i
 
             if solve(board):
@@ -87,7 +84,7 @@ def solve(board):
 
     return False
 
-
-# print_fancier_board(alt_board)
-solve_sudoku(alt_board)
-
+print_board(board)
+solve(board)
+print("---------------------------------")
+print_board(board)
